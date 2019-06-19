4c101fda0800caf860d0a7524eed132a72a23109
c00k133 <axel.neergaard@smartly.io>
2019-06-19 22:40:52 +0300

    Start frontend

--- fotogalleri/gallery/views.py
+++ fotogalleri/gallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    template = 'base.html'
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template, context)
