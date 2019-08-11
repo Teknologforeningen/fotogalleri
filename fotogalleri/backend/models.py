297068f93b94b2a9d51c08df9eec80f5970c7cda
Axel Neergaard <axel.neergaard@aalto.fi>
2019-08-11 03:49:46 +0300

    Add Django model for image metadata

--- fotogalleri/backend/models.py
+++ fotogalleri/backend/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+def new_image_path(instance, filename):
+    """
+    Image will be uploaded to MEDIA_ROOT/<input_dirs>/<filename>
+    """
+    return '{input_dirs}/{filename}'.format(input_dirs=instance.input_dirs, filename=filename)
+
+
+class ImageMetadata(models.Model):
+    '''
+    Metadata model for uploaded images.
+
+    Stores simple metadata such as file name, path, and time and date of upload.
+    '''
+    filename = models.CharField(max_length=255)
+    image_path = models.ImageField(upload_to=new_image_path)
+    upload_time = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __init__(self, img_width_field=None, img_height_field=None, *args, **kwargs):
+        self.width_field = img_width_field
+        self.height_field = img_height_field
+        super(ImageMetadata, self).__init__(*args, **kwargs)
+
+    def _get_width(self):
+        width = self.width_field
+        if not width:
+            try:
+                width = self.image_path.width_field
+            except Exception as e:
+                raise ValueError(
+                        'Save ImageMetadata object for getting width. Alternatively provide <img_width_field>')
+        return width
+
+    def _get_height(self):
+        height = self.height_field
+        if not height:
+            try:
+                height = self.image_path.height_field
+            except Exception as e:
+                raise ValueError(
+                        'Save ImageMetadata object for getting height. Alternatively provide <img_height_field>')
+        return height
+
+    width = property(_get_width)
+    height = property(_get_height)
+
+    def __str__(self):
+        return '{name};{path};{w}x{h};{time}'.format(name=self.filename,
+                                                     path=self.image_path,
+                                                     w=self.width,
+                                                     h=self.height,
+                                                     time=self.upload_time)
