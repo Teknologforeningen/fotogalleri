36f106d08cae0e1eb4c3e945afca457793d866d2
Axel Neergaard <axel.neergaard@aalto.fi>
2019-09-15 20:57:39 +0300

    Fix saving issues in Thumbnail Queue Image Object

--- fotogalleri/backend/thumbnail/thumbnail_queue_image_object.py
+++ fotogalleri/backend/thumbnail/thumbnail_queue_image_object.py
@@ -1,6 +1,7 @@
 from backend.models import ImageMetadata
 from backend.thumbnail.thumbnail_utils import generate_thumbnails, save_to_path
 from ntpath import split
+from os.path import join, splitext
 from logging import getLogger
 
 
@@ -10,7 +11,6 @@ logger = getLogger(__name__)
 class _ThumbnailObject():
     def __init__(self, thumbnail):
         self.thumbnail = thumbnail
-        self.dimension = thumbnail.size[0]
         self._done = False
 
     def set_done(self):
@@ -18,6 +18,9 @@ class _ThumbnailObject():
 
     def is_done(self):
         return self._done
+
+    def get_dimensions(self):
+        return self.thumbnail.size
 
 
 class ThumbQueueImageObject():
@@ -33,7 +36,7 @@ class ThumbQueueImageObject():
             _ThumbnailObject(thumbnail)
             for thumbnail in
             generate_thumbnails(
-                self.metadata.image_path,
+                self.metadata.image.path,
                 ThumbQueueImageObject._DEFAULT_MINSIZES,
                 ThumbQueueImageObject._DEFAULT_MAXSIZES)
         ]
@@ -43,25 +46,27 @@ class ThumbQueueImageObject():
             try:
                 save_to_path(thumbnail_object.thumbnail, filename, path)
             # TODO: set specific exception
-            except Exception:
-                logger.error('Could not save thumbnail for {}'.format(thumbnail_object.thumbnail))
+            except Exception as error:
+                logger.error('Could not save thumbnail for {}, reason: {}'.format(thumbnail_object.thumbnail, error))
                 return False
             thumbnail_object.set_done()
             return True
         return save_thumbnail
 
     def _save_thumbnails(self, thumbnail_objects):
-        filename, path = self.get_image_name_and_path()
+        raw_path, raw_filename = self._get_image_name_and_path()
+        path = join(raw_path, 'thumbnails')
+        filename, _ = splitext(raw_filename)
         save_thumbnail_to_path = self._init_save_thumbnail(filename, path)
         return filter(lambda thumbnail_object: save_thumbnail_to_path(thumbnail_object), thumbnail_objects)
 
     def generate_thumbnails(self):
         thumbnail_objects = self._create_thumbnail_objects()
         saved_thumbnails = self._save_thumbnails(thumbnail_objects)
-        self.metadata.set_thumbnails(saved_thumbnails)
+        self.metadata.set_thumbnails([saved_thumbnail.get_dimensions() for saved_thumbnail in saved_thumbnails])
 
     def get_full_image_path(self):
-        return self.metadata.image.image_url.url
+        return self.metadata.image.path
 
     def _get_image_name_and_path(self):
         '''
