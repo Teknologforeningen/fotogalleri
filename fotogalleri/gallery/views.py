acbb5589852240d750425e13b75dc1d3b3719703
Axel Neergaard <axel.neergaard@aalto.fi>
2019-09-15 19:38:04 +0300

    Add generic views for uploading and viewing images

--- fotogalleri/gallery/views.py
+++ fotogalleri/gallery/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 from django.views import View
+from django.urls import reverse_lazy
+from backend.models import ImageMetadata
+from gallery.forms import ImageUploadForm
 
 
 class HomeView(View):
@@ -8,3 +12,15 @@ class HomeView(View):
     def get(self, request):
         context = {}
         return render(request, self.template, context)
+
+
+class ImageView(ListView):
+    model = ImageMetadata
+    template_name = 'view_images.html'
+
+
+class ImageUploadView(CreateView):
+    model = ImageMetadata
+    form_class = ImageUploadForm
+    template_name = 'upload_image.html'
+    success_url = reverse_lazy('view')
