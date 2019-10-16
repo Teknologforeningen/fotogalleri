4ccd656be09f361736174edbd98419fefd15ea9d
Axel Neergaard <axel.neergaard@gmail.com>
2019-10-16 17:54:54 +0300

    Revert "Frontend revamp"

--- fotogalleri/gallery/static/javascript/upload.js
+++ fotogalleri/gallery/static/javascript/upload.js
@@ -1,31 +1,19 @@
 $(function() {
-    $('.js-upload-photos').click(function() {
-        $('#uploaded-file-path').val(
-            $('#uploaded-file-path').val()
-            || window.location.pathname.split('/').slice(2).join('/')
-        );
-
-        $('#fileupload').click();
+    $(".js-upload-photos").click(function() {
+        $("#fileupload").click();
     });
 
-    $('#fileupload').fileupload({
+    $("#fileupload").fileupload({
         dataType: 'json',
         formData: function() {
-            return $('#upload-form').serializeArray();
+            return $('#upload-form').serializeArray()
         },
         url: '/upload/',
         done: function(e, data) {
             if (data.result.is_valid) {
-                const { url, name, width, height } = data.result;
-                const widthratio = width / height * 300;
-                const padding = height / width * 100;
-
-                $('#all-images').append(`
-                    <a href="${url}" class="img-link" style="width: ${widthratio}px; flex-grow: ${widthratio}px;">
-                      <i style="padding-bottom: ${padding}%"></i>
-                      <img class="thumbnail-small" src="${url}" alt="${name}" />
-                    </a>
-                `);
+                console.log(data.result.url);
+            } else { 
+                console.log('NOPE');
             }
         }
     });
