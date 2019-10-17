cebd1e8fac679410555fc98aa605e15c4e5c0c54
cookie <axel.neergaard@gmail.com>
2019-10-17 21:46:12 +0300

    Add models for featuregates

--- fotogalleri/gallery/models.py
+++ fotogalleri/gallery/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.db.models import Model, CharField, ManyToManyField
+from django.contrib.auth.models import Group
 
-# Create your models here.
+
+class FeatureGate(Model):
+    name = CharField(max_length=256, blank=False, null=False)
+    feature_groups = ManyToManyField(Group, blank=False, related_name='featuregate')
