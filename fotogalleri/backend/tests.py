003cbd8f9ba57583e8ec427998ab01a1f52a95ba
cookie <axel.neergaard@gmail.com>
2019-11-23 01:32:51 +0200

    Add smoke tests for backend/models.py

---
+++ fotogalleri/backend/tests.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from backend.models import ImageMetadata, RootImage
+from backend.models import ImagePath, RootPath
+from fotogalleri.settings import BASE_DIR
+from os.path import join
+
+
+class ImageMetadataTest(TestCase):
+    def setUp(self):
+        image_path = join(BASE_DIR, 'backend', 'fixtures', 'test.png')
+        self.image = ImageMetadata.objects.create(image=image_path)
+
+    def test_setting_thumbnails_succeds(self):
+        test_thumbnails = [[25, 25], [40, 40]]
+        self.image.set_thumbnails(test_thumbnails)
+        self.assertEqual(self.image.thumbnails, test_thumbnails)
+
+    def test_setting_thumbnails_fails(self):
+        test_thumbnails = None
+        with self.assertRaises(TypeError):
+            self.image.set_thumbnails(test_thumbnails)
+
+    def test_root_image(self):
+        root_image = RootImage.objects.get(image_metadata=self.image)
+        self.assertEqual(root_image.image_metadata, self.image)
+
+
+class ImagePathTest(TestCase):
+    PARENT_PATH = 'parent'
+    CHILD_PATH = 'child'
+
+    def setUp(self):
+        self.parent = ImagePath.objects.create(path=ImagePathTest.PARENT_PATH)
+        self.child = ImagePath.objects.create(parent=self.parent, path=ImagePathTest.CHILD_PATH)
+
+    def test_full_parent_path(self):
+        self.assertEqual(self.parent.full_path, ImagePathTest.PARENT_PATH)
+
+    def test_full_child_path(self):
+        full_child_path = join(ImagePathTest.PARENT_PATH, ImagePathTest.CHILD_PATH)
+        self.assertEqual(self.child.full_path, full_child_path)
+
+    def test_parent_root_path(self):
+        root_path = RootPath.objects.get(image_path=self.parent)
+        self.assertEqual(root_path.image_path, self.parent)
+
+    def test_child_root_path(self):
+        with self.assertRaises(RootPath.DoesNotExist):
+            RootPath.objects.get(image_path=self.child)
