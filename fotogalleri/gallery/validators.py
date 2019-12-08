9924004427729b2fc99f9a2e93aa8eb59eb5a56b
rosasam <samuel.rosas@aalto.fi>
2019-12-08 22:36:24 +0200

    Fix linter errors

--- fotogalleri/gallery/validators.py
+++ fotogalleri/gallery/validators.py
@@ -6,6 +6,7 @@ def validate_path_name(value):
     forbidden_strings = ['/', '\\', '.', ' ', '\'', '\"']
     if any(forbidden in value for forbidden in forbidden_strings):
         raise ValidationError(
-            _('The folder name can\'t contain any of the following characters: slashes ( / , \ ), quotation marks ( \", \'),  dots, spaces'),
+            _('The folder name can\'t contain any of the following characters:' +
+              'slashes ( / , \\ ), quotation marks ( \", \'),  dots, spaces'),
             params={'value': value},
         )
